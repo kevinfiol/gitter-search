d8219b5054ef0700a2185005fb863ac02ace4fc1
kevinfiol <me@kevinfiol.com>
2022-02-18 20:28:36 -0500

    update bundle script

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -6,6 +6,9 @@ env.load();
 export const ENTRY = resolve('src/client/index.js');
 export const OUTFILE = resolve('dist/app.js');
 
+const SERVER_ADDRESS = process.env.SERVER_ADDRESS;
+const SERVER_PORT = process.env.SERVER_PORT;
+
 export function bundle(config = {}) {
   return esbuild.build({
     format: 'iife',
@@ -15,7 +18,7 @@ export function bundle(config = {}) {
     jsxFactory: 'm',
     jsxFragment: '"["',
     define: {
-        'process.env.API_URL': `"${process.env.SERVER_ADDRESS}:${process.env.SERVER_PORT}"`
+        'process.env.API_URL': `"${SERVER_ADDRESS}${SERVER_PORT ? `:${SERVER_PORT}` : ''}"`
     },
     ...config
   });
